cd5a17eed1fcbf5675019c47cfb33fc645f6bd15
ReneDouglas <rene.douglas.bsi@gmail.com>
2025-08-21 13:25:22 -0300

    feat: add avaliaif-backend

---
+++ apps/avaliaif-backend/src/test/java/br/edu/ifpi/avaliaif_backend/AvaliaifBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpi.avaliaif_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliaifBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
