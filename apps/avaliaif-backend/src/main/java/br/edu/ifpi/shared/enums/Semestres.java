17d24af3ffdc4e6e99623430392367212b08ec0a
ReneDouglas <rene.douglas.bsi@gmail.com>
2025-09-17 21:43:01 -0300

    feat: add enums

---
+++ apps/avaliaif-backend/src/main/java/br/edu/ifpi/shared/enums/Semestres.java
@@ -0,0 +1,11 @@
+package br.edu.ifpi.shared.enums;
+
+/**
+ * Enumeração que representa os semestres acadêmicos.
+ *
+ * @author Renê Morais
+ */
+public enum Semestres {
+  PRIMEIRO,
+  SEGUNDO,
+}
---
+++ apps/avaliaif-backend/src/main/java/br/edu/ifpi/shared/enums/TiposDisciplina.java
@@ -0,0 +1,11 @@
+package br.edu.ifpi.shared.enums;
+
+/**
+ * Enumeração que representa os tipos de disciplina.
+ *
+ * @author Renê Morais
+ */
+public enum TiposDisciplina {
+  TECNICA,
+  PROPEDEUTICA,
+}
