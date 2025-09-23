d7ad2f9836e2b440f1ed20eea309e08826fb407d
jhonatasjgr <jhon.gomes.r@gmail.com>
2025-09-23 15:43:12 -0300

    feat: criando base repository servidor

---
+++ apps/avaliaif-backend/src/main/java/br/edu/ifpi/avaliaifapi/servidor/ServidorRepository.java
@@ -0,0 +1,13 @@
+package br.edu.ifpi.avaliaifapi.servidor;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Repositório para a entidade Servidor.
+ *
+ * @author Jhonatas G. Ribeiro
+ */
+
+public interface ServidorRepository extends JpaRepository<Servidor, Long> {
+
+}
